927d4e7f0fe8d1bf788c8923abb8e44dde6cc862
roshinsthomas <roshinsthomas@gmail.com>
2026-08-29 00:09:42 +0530

    Added CMS backend apps and DRF setup

--- clinic_project/settings.py
+++ clinic_project/settings.py
@@ -40,6 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'receptionist',
+    'doctor',
+    'laboratory',
+    'pharmacy',
+    'accounts',
 ]
 
 MIDDLEWARE = [
