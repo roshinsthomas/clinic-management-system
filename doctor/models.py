c3b36f11f9eb8c944aa450fdd8a32f98ea937369
roshinsthomas <roshinsthomas@gmail.com>
2026-08-30 20:06:29 +0530

    Doctor Module: Built Consultation model

--- doctor/models.py
+++ doctor/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Consultation(models.Model):
+    consultation_id = models.AutoField(primary_key=True)
+
+    appointment = models.OneToOneField(
+        'receptionist.Appointment',
+        on_delete=models.CASCADE
+    )
+
+    symptoms = models.TextField()
+    diagnosis = models.TextField()
+
+    notes = models.TextField(
+        null=True,
+        blank=True
+    )
+
+    consultation_date = models.DateField(
+        auto_now_add=True
+    )
+
+    locked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Consultation {self.consultation_id}"
